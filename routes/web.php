1fab1fad568f1f33994788e9720e8b8f25ebf11c
Darivs <darivsxp@gmail.com>
2026-02-10 22:00:53 +0800

    Version 2.1

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,6 @@ use App\Http\Controllers\Auth\SocialAuthController;
 
 Route::get('/', function () {
     return redirect()->route('login');
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
 });
 
 // Route::get('/dashboard', function () {
