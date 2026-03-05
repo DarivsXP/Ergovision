32efa9f86172ae3e4df89ef54906fd744420630e
Darivs <darivsxp@gmail.com>
2026-03-05 13:11:23 +0800

    Error fixing in session feedback

--- app/Models/SessionFeedback.php
+++ app/Models/SessionFeedback.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SessionFeedback extends Model
 {
+    protected $table = 'session_feedbacks'; 
+
     protected $fillable = [
         'user_id',
         'fatigue_level',
